0af42af602539bb300465b82ff67aafd8ab83185
Alexandr Patsura <alexandrpatsura@gmail.com>
2018-01-17 22:38:35 +0200

    del unnecessary folders

--- SlickSliderCustomDots/slickDots.js
+++
@@ -1,8 +0,0 @@
-// moving Dots in the Bootstrap container
-
-$('.slider_class').slick({
-	dots: true,
-	dotsClass: 'dots_class container', // add your class and bootstrap class
-	arrows: false,
-	responsive: [
-});
